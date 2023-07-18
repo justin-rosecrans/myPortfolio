154abfc1646c5e4c881c5ed0890478883469432f
justin-rosecrans <jrscrns@gmail.com>
2023-07-18 13:50:18 -0500

    footer update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './components/navBar'
+import Footer from './components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className="bg-slate-800">
         <NavBar />
         {children}
+        <Footer />
         </body>
     </html>
   )
