30916ec301bca2a9631e27f02d78a6c8078cf8c1
justin-rosecrans <jrscrns@gmail.com>
2023-07-18 11:43:33 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
-/**
- * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
- */
-
 const nextConfig = {
   trailingSlash: true,
   output: 'export'
 }
-
-module.exports = {
-  content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{ts,tsx}",
-    "./public/**/*.html",
-  ],
-  plugins: [
-    require("flowbite/plugin"),
-    nextConfig
-  ],
-  theme: {},
-};
+module.exports = nextConfig
