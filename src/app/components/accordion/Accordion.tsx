ed01a35aba6355e319850ef9a94c256eed12f262
Justin Rosecrans <justin.rosecrans@gmail.com>
2023-08-23 16:27:02 -0500

    update to the nav links hover - added some info to the portfolio accordion

--- src/app/components/accordion/Accordion.tsx
+++ src/app/components/accordion/Accordion.tsx
@@ -13,16 +13,34 @@ export default function CollapseAll() {
         <Accordion.Content>
         <ul className="mb-2 text-gray-500 dark:text-gray-400">
             <li className="mb-1">
-              <b>Projects Showcase:</b> Highlight your best and most diverse projects,
-              including websites, web applications, and any other relevant work.
+              <b>Projects Showcase:</b>
+              <p className='py-2'>
+                This project was a complete redesign of the Schwab Education Center. The goal was to create a more modern and responsive
+                design that would work on mobile and desktop. The project was built on a legacy framework of Angular1 and was a challenge to
+                make it work responsively.
+              </p>
             </li>
             <li className="mb-1">
-              <b>Case Studies:</b> Detailed breakdowns of your projects, showcasing
-              challenges, solutions, and your role in each project.
+              <b>Case Studies:</b> 
+              <p className='py-2'>
+                The challenging part of this project was to create a carousel that would work responsively for mobile on a
+                legacy framework of Angular1. Not many javascript libraries support Angular1 anymore and the ones that do
+                are not very well maintained. So most of the work was done with vanilla javascript.
+              </p>
+              <p className='py-2'>
+                The solution was to use Slick.js and create a custom directive to make it work 
+                with Angular1. I then added some custom animations and transitions with CSS to make it look more appealing.
+              </p>
+              <p className='py-2'>
+                I was the sole developer for the front end and UI of this project and worked with a team of 3 other developers who did
+                the back end and API work.
+              </p>
             </li>
-            <li className="mb-1">
-              <b>Technologies Used:</b> Specify the technologies, languages,
-              frameworks, and tools you used for each project.
+            <li className="py-2">
+              <b>Technologies Used:</b>
+              <p>
+                AngularJS, SASS/CSS, HTML5, JavaScript, Grunt, CSS Animation, Slick.js, and Maven
+              </p>
             </li>
           </ul>
         </Accordion.Content>
--- src/app/components/carousel/Carousel.tsx
+++ src/app/components/carousel/Carousel.tsx
@@ -6,7 +6,7 @@ export default function slideShowCarousel() {
   return (
     <Carousel
       data-carousel="slide"
-      slideInterval={500000}
+      slideInterval={5000}
       className="relative w-full h-96 z-0"
     >
       <div>
@@ -27,67 +27,75 @@ export default function slideShowCarousel() {
           </div>
         </div>
       </div>
-      <Image
-        src="/carousel/Schwab/global_translations.svg"
-        className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
-        width={500}
-        height={500}
-        alt="..."
-      />
-      <div className="w-full bg-gradient-to-t from-black">
-        <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
-        <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold">Project Two</h3>
-            <p className="text-sm">2023</p>
+      <div>
+        <Image
+          src="/carousel/Schwab/Education_Centre_Hover.svg"
+          className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
+          width={500}
+          height={500}
+          alt="..."
+        />
+        <div className="w-full bg-gradient-to-t from-black">
+          <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
+          <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
+            <div className="text-center">
+              <h3 className="text-2xl font-bold">Project One</h3>
+              <p className="text-sm">2023</p>
+            </div>
           </div>
         </div>
       </div>
-      <Image
-        src="/carousel/Schwab/Education_Centre_Hover.svg"
-        className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
-        width={500}
-        height={500}
-        alt="..."
-      />
-      <div className="w-full bg-gradient-to-t from-black">
-        <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
-        <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold">Project Three</h3>
-            <p className="text-sm">2023</p>
+      <div>
+        <Image
+          src="/carousel/Schwab/global_translations.svg"
+          className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
+          width={500}
+          height={500}
+          alt="..."
+        />
+        <div className="w-full bg-gradient-to-t from-black">
+          <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
+          <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
+            <div className="text-center">
+              <h3 className="text-2xl font-bold">Project Two</h3>
+              <p className="text-sm">2023</p>
+            </div>
           </div>
         </div>
       </div>
-      <Image
-        src="/carousel/Schwab/global_translations.svg"
-        className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
-        width={500}
-        height={500}
-        alt="..."
-      />
-      <div className="w-full bg-gradient-to-t from-black">
-        <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
-        <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold">Project Four</h3>
-            <p className="text-sm">2023</p>
+      <div>
+        <Image
+          src="/carousel/Schwab/global_translations.svg"
+          className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
+          width={500}
+          height={500}
+          alt="..."
+        />
+        <div className="w-full bg-gradient-to-t from-black">
+          <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
+          <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
+            <div className="text-center">
+              <h3 className="text-2xl font-bold">Project Four</h3>
+              <p className="text-sm">2023</p>
+            </div>
           </div>
         </div>
       </div>
-      <Image
-        src="/carousel/Schwab/Education_Centre_Hover.svg"
-        className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
-        width={500}
-        height={500}
-        alt="..."
-      />
-      <div className="w-full bg-gradient-to-t from-black">
-        <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
-        <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold">Project Five</h3>
-            <p className="text-sm">2023</p>
+      <div>
+        <Image
+          src="/carousel/Schwab/Education_Centre_Hover.svg"
+          className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-screen-md"
+          width={500}
+          height={500}
+          alt="..."
+        />
+        <div className="w-full bg-gradient-to-t from-black">
+          <div className="w-full h-24 bg-gradient-to-t from-black opacity-50"></div>
+          <div className="absolute bottom-0 left-0 w-full h-24 flex items-center justify-center text-white">
+            <div className="text-center">
+              <h3 className="text-2xl font-bold">Project Five</h3>
+              <p className="text-sm">2023</p>
+            </div>
           </div>
         </div>
       </div>
